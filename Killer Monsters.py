302158951474386c6f44649d488e4630e987871f
Ansh Sharma <122905517+AnshSharma16@users.noreply.github.com>
2024-06-18 15:49:02 +0530

    Killer Monsters.py

--- Killer Monsters.py
+++ Killer Monsters.py
@@ -16,7 +16,7 @@ for _ in range(t):
     
     for i in range(n):
         battlefield.append(strength[i])
-        count = count_and_remove_smaller_than_last(battlefield)
+        count_and_remove_smaller_than_last(battlefield)
         remaining.append(count)
     
     print(" ".join(map(str, remaining)))
