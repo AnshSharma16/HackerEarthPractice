7c88daaf6f62081af01a84f797cd1dba5097afc0
Ansh Sharma <122905517+AnshSharma16@users.noreply.github.com>
2024-06-18 15:54:45 +0530

    Killer Monsters.py
    Problem
    
    Given an array 
     denoting strengths of 
     monsters, we start with an empty battlefield, at each minute  i, the  i th monster joins the battlefield and kills all monsters whose strength is less than or equal to his strength.
    
    Find the number of monsters alive in the battlefield at end of 
    i th  minute.
    
    NOTE:
    
    
    Input Format:
    
    First line contains an integer T, the number of test cases.
    First line of each testcase contains an integer N, the number of monsters..
    Second line of each testcase contains N space seperated integers, the strengths of N monsters.
    
    
    Output Format:
    
    For each testcase, output 
     integers denoting the number of monsters alive after ith
     minute .
    
    
    Sample Input
    3
    5
    3 0 3 4 1
    5
    5 4 3 2 1
    7
    1 2 3 3 4 4 0
    
    Sample Output
    1 2 1 1 2
    1 2 3 4 5
    1 1 1 1 1 1 2

--- Killer Monsters.py
+++ Killer Monsters.py
@@ -16,7 +16,7 @@ for _ in range(t):
     
     for i in range(n):
         battlefield.append(strength[i])
-        count_and_remove_smaller_than_last(battlefield)
+        count= count_and_remove_smaller_than_last(battlefield)
         remaining.append(count)
     
     print(" ".join(map(str, remaining)))
